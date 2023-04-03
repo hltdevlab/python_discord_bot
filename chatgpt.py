d8ad89b79502a03de4773571085634e66a85e09e
hltdevlab <54474336+hltdevlab@users.noreply.github.com>
2023-04-03 13:37:20 +0800

    Update chatgpt.py

--- chatgpt.py
+++ chatgpt.py
@@ -28,8 +28,10 @@ Response example:
 def ask_chatgpt(message):
     print('sending to openai...')
     response = openai.Completion.create(
-        model = "text-davinci-003",
-        prompt = "The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: Hello, who are you?\nAI: I am an AI created by OpenAI. How can I help you today?\nHuman: I'd like to cancel my subscription.\nAI:",
+        #model = "text-davinci-003",
+        model = "gpt-3.5-turbo",
+        #prompt = "The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: Hello, who are you?\nAI: I am an AI created by OpenAI. How can I help you today?\nHuman: I'd like to cancel my subscription.\nAI:",
+        prompt = message,
         temperature = 0.9,
         max_tokens = 150,
         top_p = 1,
