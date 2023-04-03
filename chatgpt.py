96eb0a2e3cb85c1408785f7d81233d901a94dc9d
hltdevlab <54474336+hltdevlab@users.noreply.github.com>
2023-04-03 13:29:57 +0800

    Update chatgpt.py

--- chatgpt.py
+++ chatgpt.py
@@ -26,6 +26,7 @@ Response example:
 '''
 
 def ask_chatgpt(message):
+    print('sending to openai...')
     response = openai.Completion.create(
         model = "text-davinci-003",
         prompt = "The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: Hello, who are you?\nAI: I am an AI created by OpenAI. How can I help you today?\nHuman: I'd like to cancel my subscription.\nAI:",
@@ -36,4 +37,6 @@ def ask_chatgpt(message):
         presence_penalty = 0.6,
         stop = [" Human:", " AI:"]
     )
-    return response['choices'][0]['message']['content']
+    print('response: ' + str(response))
+    print('msg content: ' + str(response['choices'][0]['message']['content']))
+    return str(response['choices'][0]['message']['content'])
