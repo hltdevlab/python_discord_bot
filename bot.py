e257b146b77fc1843f0d2ea30a6572d9204bc2fc
hltdevlab <54474336+hltdevlab@users.noreply.github.com>
2023-04-03 19:43:03 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,9 +5,9 @@ from dotenv import dotenv_values
 config = dotenv_values(".env")
 
 
-async def send_message(message, user_message, is_private):
+async def send_message(message, user_message, is_private, formatted_history_messages=[]):
     try:
-        response = responses.handle_response(user_message)
+        response = responses.handle_response(user_message, formatted_history_messages=formatted_history_messages)
         await message.author.send(response) if is_private else await message.channel.send(response)
     except Exception as e:
         print(e)
@@ -58,15 +58,15 @@ def run_discord_bot():
             user_message = str(message.content)
             channel = str(message.channel)
 
-            history_messages = await get_history(message.channel)
+            formatted_history_messages = await get_history(message.channel)
 
             print(f"{username} said: '{user_message}' ({channel})")
 
             if user_message[0] == '?':
                 user_message = user_message[1:]
-                await send_message(message, user_message, is_private=True)
+                await send_message(message, user_message, is_private=True, formatted_history_messages=formatted_history_messages)
             else:
-                await send_message(message, user_message, is_private=False)
+                await send_message(message, user_message, is_private=False, formatted_history_messages=formatted_history_messages)
 
 
     client.run(TOKEN)
