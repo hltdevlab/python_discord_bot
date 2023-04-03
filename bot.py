873c1b7066889e13ac8797371fb7569f9532aa82
hltdevlab <54474336+hltdevlab@users.noreply.github.com>
2023-04-03 16:25:23 +0800

    Added send typing

--- bot.py
+++ bot.py
@@ -37,22 +37,23 @@ def run_discord_bot():
 
     @client.event
     async def on_message(message):
-        if message.author == client.user:
-            return
+        async with message.channel.typing():
+            if message.author == client.user:
+                return
 
-        username = str(message.author)
-        user_message = str(message.content)
-        channel = str(message.channel)
+            username = str(message.author)
+            user_message = str(message.content)
+            channel = str(message.channel)
 
-        history_messages = await get_history(message.channel)
+            history_messages = await get_history(message.channel)
 
-        print(f"{username} said: '{user_message}' ({channel})")
+            print(f"{username} said: '{user_message}' ({channel})")
 
-        if user_message[0] == '?':
-            user_message = user_message[1:]
-            await send_message(message, user_message, is_private=True)
-        else:
-            await send_message(message, user_message, is_private=False)
+            if user_message[0] == '?':
+                user_message = user_message[1:]
+                await send_message(message, user_message, is_private=True)
+            else:
+                await send_message(message, user_message, is_private=False)
 
 
     client.run(TOKEN)
