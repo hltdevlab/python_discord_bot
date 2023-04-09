0c2c0d4b3f68ce9c8e83f55d17c2069e70d02db1
hltdevlab <54474336+hltdevlab@users.noreply.github.com>
2023-04-09 20:21:19 +0800

    added config init

---
+++ config.py
@@ -0,0 +1,24 @@
+import system_message
+from dotenv import dotenv_values
+
+env = {}
+runtime = {
+    'bot_name': '',
+    'system_message': ''
+}
+
+
+def init():
+    env = dotenv_values(".env")
+    print('config initialised.')
+
+
+def get_system_message():
+    if runtime['bot_name'] == '':
+        return ''
+
+    # load system message from file if it has not been initialised    
+    if runtime['system_message'] == '':
+        runtime['system_message'] = system_message.load(bot_name=bot_name)
+    
+    return runtime['system_message']
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import bot
+import config
 
 if __name__ == '__main__':
+    config.init()
     bot.run_discord_bot()
