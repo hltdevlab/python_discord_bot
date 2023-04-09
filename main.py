37c9ea5feb8c22e4fe026a205533eb6e1c27779f
hltdevlab <54474336+hltdevlab@users.noreply.github.com>
2023-04-09 20:31:20 +0800

    debug prints

--- main.py
+++ main.py
@@ -5,4 +5,7 @@ if __name__ == '__main__':
     config.init()
     print(config.env)
     print(config.runtime)
+    config.runtime['bot_name'] = 'test'
+    config.get_system_message()
+    print(config.runtime['system_message'])
     bot.run_discord_bot()
