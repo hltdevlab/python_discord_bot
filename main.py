ad7c5980b5f418b8529220455907f30c44132395
hltdevlab <54474336+hltdevlab@users.noreply.github.com>
2023-04-09 21:52:32 +0800

    clean debug prints

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import bot
 import config
 
 if __name__ == '__main__':
-    # config.init()
-    print(f"env: {config.env}")
-    print(f"runtime: {config.runtime}")
+    # print(f"env: {config.env}")
+    # print(f"runtime: {config.runtime}")
     bot.run_discord_bot()
