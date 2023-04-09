af5a66d930c3b88f2f0517554ecf3d83cf59bf7d
hltdevlab <54474336+hltdevlab@users.noreply.github.com>
2023-04-09 09:57:16 +0800

    Create system_message.py

---
+++ system_message.py
@@ -0,0 +1,14 @@
+import datetime
+
+def read_system_message(file_path='system_message.txt', bot_name=''):
+    try:
+        with open(file_path, 'r') as file:
+            system_message = file.read()
+            datetime_now = datetime.datetime.now().strftime("%d %b %Y %H:%M:%S")
+            formatted_text = system_message.format(bot_name=bot_name, datetime_now=datetime_now)
+            return formatted_text
+        
+    except FileNotFoundError:
+        error_message = "Error: The specified file '{}' does not exist. Returning empty string.".format(file_path)
+        print(error_message)
+        return ''
