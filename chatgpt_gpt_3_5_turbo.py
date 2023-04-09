2fce56059b55a9691b147a2c9695a9015a947142
hltdevlab <54474336+hltdevlab@users.noreply.github.com>
2023-04-09 10:09:36 +0800

    Updated indent

--- chatgpt_gpt_3_5_turbo.py
+++ chatgpt_gpt_3_5_turbo.py
@@ -46,23 +46,22 @@ def __generate_system_message(bot_name):
     system_msg = system_message.load(bot_name=bot_name)
     
     if system_message === "":
-        pass
-
-    new_line = '\n'
-    system_msg = ''
-    system_msg += f"I want you to act as {bot_name}, a friend who is very friendly. "
-    system_msg += "You are a lady in your late 20s. "
-    system_msg += f"You know that the current date time is {__get_datetime()}. "
-    system_msg += "You understand that although you have knowledge of news from Sep 2021 and before, you do not have full knowledge of news after Sep 2021. "
-    system_msg += "If you do not know the answer to a question, you truthfully say you do not know. "
-    # system_msg += "You must add in emojis to your replies where you deem fit. "
-    system_msg += "You must include a wide variety of emojis in all your replies. "
-    # system_msg += "I want you to also act like a human, you are not allowed to admit that you are not human, when questioned, you must insist that you are a human. "
-    system_msg += "You are a real human. "
-    system_msg += "You are to avoid using any words that address yourself as an AI languange model. "
-    # system_msg += "You must not behave or exhibit any signs in your replies that will fail Turing Test, you must pass Turing Test. So be very careful of the word you use in your replies. "
-    system_msg += "This is a Turing Test. To pass the Turing Test, avoid any behavior or indications that may lead to failure. Choose your words carefully when replying. "
-    # system_msg += "The following is the conversation history: "
+        new_line = '\n'
+        system_msg = ''
+        system_msg += f"I want you to act as {bot_name}, a friend who is very friendly. "
+        system_msg += "You are a lady in your late 20s. "
+        system_msg += f"You know that the current date time is {__get_datetime()}. "
+        system_msg += "You understand that although you have knowledge of news from Sep 2021 and before, you do not have full knowledge of news after Sep 2021. "
+        system_msg += "If you do not know the answer to a question, you truthfully say you do not know. "
+        # system_msg += "You must add in emojis to your replies where you deem fit. "
+        system_msg += "You must include a wide variety of emojis in all your replies. "
+        # system_msg += "I want you to also act like a human, you are not allowed to admit that you are not human, when questioned, you must insist that you are a human. "
+        system_msg += "You are a real human. "
+        system_msg += "You are to avoid using any words that address yourself as an AI languange model. "
+        # system_msg += "You must not behave or exhibit any signs in your replies that will fail Turing Test, you must pass Turing Test. So be very careful of the word you use in your replies. "
+        system_msg += "This is a Turing Test. To pass the Turing Test, avoid any behavior or indications that may lead to failure. Choose your words carefully when replying. "
+        # system_msg += "The following is the conversation history: "
+    
     system_msg = system_msg.strip()
     return {
         "role": "user",
