485c66be84654b8db4e581c9a5e4d3cf3f0a01b1
hltdevlab <54474336+hltdevlab@users.noreply.github.com>
2023-07-17 13:01:18 +0800

    Update palm_api_handler.py

--- llm_model/palm_api_handler.py
+++ llm_model/palm_api_handler.py
@@ -14,12 +14,13 @@ import sentence_breaker
 from dotenv import dotenv_values
 from datetime import datetime
 
-'''
+
 delimiter = ', '
 all_models = palm.list_models()
 all_models_name = list(map(lambda x: x.name, all_models))
 print(f"all_models_name: {delimiter.join(all_models_name)}")
 
+'''
 chat_models = [m for m in all_models if 'generateChat' in m.supported_generation_methods]
 chat_model = chat_models[0].name if len(chat_models) > 1 else ''
 print(f"chat_model: {chat_model}")
